5eef6449bd7af1725d5d1cb6fb5e57afdf7e8145
Apple <riyabulia12@gmail.com>
2022-09-13 22:27:09 +0530

    Create navigation menu with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Calculator from './Components/Calculator';
+import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Calculator</h1>
-        <Calculator />
+        <nav className="navigation-menu">
+          <h1 className="app-title">Math Magicians</h1>
+          <ul className="menu-list">
+            <li id="list"> Home </li>
+            <li> | </li>
+            <li id="create">Calculator</li>
+            <li> | </li>
+            <li id="contact">Quote </li>
+          </ul>
+        </nav>
+        <React.StrictMode>
+          <Router>
+            <Home />
+          </Router>
+        </React.StrictMode>
+
       </header>
     </div>
   );
